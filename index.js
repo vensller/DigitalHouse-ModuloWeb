cedfff0d1a3dc08732cce61b2c98ee0aeefa8295
Ivens Muller <ivensdiegomuller@gmail.com>
2021-03-03 20:42:12 -0300

    Adicionado hello world

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const app = express();
 
+app.get('/', (req, res) => {
+    return res.send('Hello world');    
+});
+
 app.listen(3000, () => {
     console.log('Servidor rodando na porta 3000');
 });
