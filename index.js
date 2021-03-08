bfd4c04334582dd6e78e2cb10c96c494b1c37f65
Ivens Diego Müller <ivensdiegomuller@gmail.com>
2021-03-08 19:21:40 -0300

    Hello world (#1)
    * Adicionado hello world
    
    * Finalizando hello-world

--- index.js
+++ index.js
@@ -1,6 +1,30 @@
 const express = require('express');
 const app = express();
 
+const listaUsuarios = [
+    'Ivens',
+    'Jailson',
+    'Maria',
+    'Natalia',
+    'Ariel',
+    'Nykolle'
+]
+
+// Query Params
+// Ex: localhost:3000/usuarios?nome=Ivens
+app.get('/usuarios', (req, res) => {
+    const { nome } = req.query;
+    let listaRetorno = listaUsuarios.filter(i => i.includes(nome || ''));
+    return res.json(listaRetorno);
+});
+
+// Route Params 
+// Ex: ecommer.com.br/produtos/4/detalhes
+app.get('/hello/:usuario', (req, res) => {    
+    const { usuario } = req.params;
+    return res.send(`Hello world ${usuario}`);
+});
+
 app.listen(3000, () => {
     console.log('Servidor rodando na porta 3000');
 });
